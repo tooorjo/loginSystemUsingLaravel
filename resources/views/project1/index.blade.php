0ab664558dac70a181ced29ef1f57e6336b92d28
Khan Sadee Mahmud <tktoor17@gmail.com>
2017-11-25 19:12:57 +0600

    using laravelCollectives/html in forms

--- resources/views/project1/index.blade.php
+++ resources/views/project1/index.blade.php
@@ -5,6 +5,7 @@
 </head>
 <body>
 <h4>Enter your username and password to login.</h4>
+<!--
 <form method="post" action="/userInfo">
 Username:
 <input type="text" name="username">
@@ -14,18 +15,19 @@ Username:
 <input type="hidden" name="_token" value="{{ csrf_token() }}">
 <input type="submit" value="submit">
 </form>
-<?php
-/*$reply="";
-session_start();
+-->
+{!! Form::open(['url'=>'/userInfo']) !!}
 
-if(isset($_SESSION['reply']))
-{
-	$reply=$_SESSION['reply'];
-}
-session_destroy();
-echo $reply."<br>";
-*/
-?>
+	{!! Form::label('username','Username:') !!}
+	{!! Form::text('username',null) !!}
+<br>
+	{!! Form::label('password','Password:') !!}
+	{!! Form::password('password',null) !!}
+<br>
+	{!! Form::submit('Log In') !!}
+
+{!! Form::close() !!}
+
 <br><br>
 <h4>Do not have an account?? <a href='register'>register</a> then, it's FREE!!!!
 </body>
--- resources/views/project1/register.blade.php
+++ resources/views/project1/register.blade.php
@@ -5,6 +5,7 @@
 </head>
 <body>
 <h4>Please fill up the given form:</h4>
+<!--
 <form method="post"  action="/project1">
 Username:
 <input type="text" name="username">
@@ -15,8 +16,22 @@ Username:
 <br>
 <input type="hidden" name="_token" value="{{ csrf_token() }}">
 <input type="submit" value="submit">
-
 </form>
+-->
+{!! Form::open(['url'=>'/project1']) !!}
+
+	{!! Form::label('username','Username:') !!}
+	{!! Form::text('username',null) !!}
+<br>
+	{!! Form::label('password','Password:') !!}
+	{!! Form::password('password',null) !!}
+<br>
+	{!! Form::label('occupation','Occupation:') !!}
+	{!! Form::text('occupation',null) !!}
+<br>
+	{!! Form::submit('Register') !!}
+
+{!! Form::close() !!}
 
 <br><br>
 <h4>Already have an account?? <a href='project1'>login</a> here.
